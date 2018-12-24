82f8211a18340dcdf9d0abd606ca13dc19056c5f
Erdem Aybek <eaybek@gmail.com>
2018-12-24 17:45:30 +0300

    installation fix

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ if [[ -z $EST_HOME ]]
 then
     export EST_HOME=$HOME/.est
 fi
-if [[ -d EST_HOME ]]
+if [[ -d $EST_HOME ]]
 then
     echo "you have already $EST_HOME"
 else
     mkdir $EST_HOME
 exit
 fi
-#git clone https://github.com/esthelpers/est $EST_HOME > /dev/null
+git clone https://github.com/esthelpers/est $EST_HOME > /dev/null
 
 
 chmod +x $EST_HOME/esthelpers.sh
